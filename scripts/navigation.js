e4e91e1286b83a27eb9e7b6c3cb022dc39b65e9d
kaylapowers <kpowers@byupathway.edu>
2025-07-08 14:50:09 -0600

    week 1 submit 1

---
+++ scripts/navigation.js
@@ -0,0 +1,11 @@
+const navLinks = document.querySelector('#nav-bar');
+const navButton = document.querySelector('#ham-btn');
+
+
+navButton.addEventListener('click',() => {
+	navButton.classList.toggle('show');
+	navLinks.classList.toggle('show');
+
+});
+
+
